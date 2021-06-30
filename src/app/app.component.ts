fd9245676e45e49f2fcc9475cb2e6f02120639c9
Popov Max <drowpmv@gmail.com>
2021-06-30 16:50:40 +0300

    added loaders, images, storybook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-app';
+  testImage = require('!file-loader!./images/test-image.png').default;
+  testIcon = require('!html-loader!image-webpack-loader!./images/test-icon.svg');
+
+  ngOnInit(): void {
+    /* const imagesContext = require.context(
+      '!html-loader!image-webpack-loader?{ svgo: { plugins: [ { removeViewBox : false }, { cleanupIDs: { minify: false } }, { convertPathData: false } ] } }!./images/',
+      true,
+      /^\.\/.*\.svg$/,
+    );
+
+    imagesContext.keys().forEach((key) => console.log('> image', key, imagesContext(key))); */
+  }
 }
---
+++ src/app/app.stories.ts
@@ -0,0 +1,12 @@
+import { Meta } from '@storybook/angular/types-6-0';
+
+import { AppComponent } from './app.component';
+
+export default {
+  title: 'App',
+  component: AppComponent,
+} as Meta;
+
+export const Primary = () => ({
+  props: {},
+});
